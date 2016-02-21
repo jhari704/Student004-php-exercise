1ed4d155750340d9aee0fde714313b21abc6823f
jhari704 <jhari704@gmail.com>
2016-02-20 20:55:52 -0600

    example 1

--- example1.php
+++ example1.php
@@ -1,1 +1,7 @@
 <?php
+/* Array Declaration*/
+$cars = array("Volvo", "BMW", "Toyota"); 
+/* Printing the array values*/
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+?>
+/* Output: I like Volvo, BMW and Toyota.*/
